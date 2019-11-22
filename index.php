33f76ca404f11fd45f555585ced941b81701bd87
Admin <admin@Admins-Mac-mini.local>
2019-11-23 01:48:43 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\User;
+
+class Profile extends Controller
+{
+	/**
+	 * Show the profile for the given user,
+	 *
+	 * @param	int	$index
+	 * @return	View
+	 */
+	public function __invoke($index)
+	{
+		return view('user.profile', ['user' => User::findOrFail($index)])
+	}
+}
