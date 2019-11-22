85980679b401ddd512655e1e215e965b856b0ec1
Admin <admin@Admins-Mac-mini.local>
2019-11-23 01:56:10 +0200

    added authorization.php

---
+++ authorization.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\Response;
+use App\Post;
+use App\User;
+
+class PostPolicy
+{
+	/**
+	 * Determine i the given post can be updated by the user.
+	 * 
+	 * @param	\App\User	$user
+	 * @param	\App\Post	$post
+	 * @return	\Illuminate\Auth\Access\Response
+	 */
+	public function update(User $user, Post $post)
+	{
+		return $user->id === $post->post_id
+				? Response::allow()
+				: Response::deny("You do not own this post")
+	}
+}
