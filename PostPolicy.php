c6ef724336aa22278f5b4df562f2ee9791e1aafd
Admin <admin@Admins-Mac-mini.local>
2019-11-23 02:23:24 +0200

    added PostPolicy.php

---
+++ PostPolicy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Policies\PostPolicy;
+use App\Post;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+	/**
+	 * The policy mapping for the application.
+	 *
+	 * @var array
+	 */
+	protected $policies = [
+		Post::class => PostPolicy::class,
+	];
+
+	/**
+	 * Register any application authentication / authorization services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->registerPolicies();
+	}
+}
