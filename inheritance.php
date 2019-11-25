b49e90c3182818c38dd93cf55a433d98257c1dde
Admin <admin@Admins-Mac-mini.local>
2019-11-25 03:19:23 +0200

    wip

--- inheritance.php
+++ inheritance.php
@@ -5,10 +5,35 @@
  */
 class Person
 {
+    /**
+     * Store the firstname field.
+     * 
+     * @var string
+     */
     private $firstName;
+
+    /**
+     * Store the lastname field.
+     * 
+     * @var string
+     */
     private $lastName;
+
+    /**
+     * Store the gender field.
+     * 
+     * @var string
+     */
     private $gender;
 
+    /**
+     * Create a new person instance
+     * 
+     * @param  string $firstName
+     * @param  string $lastName
+     * @param  string $gender
+     * @return void
+     */
     public function __construct($firstName, $lastName, $gender = 'm')
     {
         $this->firstName = $firstName;
@@ -16,6 +41,9 @@ class Person
         $this->gender = $gender;
     }
 
+    /**
+     * The (string) representation of this Person as "[firstname] [lastname]"
+     */
     public function __toString()
     {
         return $this->firstName . ' ' . $this->lastName;
@@ -27,9 +55,29 @@ class Person
  */
 class Employee extends Person
 {
+    /**
+     * Store the job field.
+     * 
+     * @var string
+     */
     private $job;
+
+    /**
+     * Store the company field.
+     * 
+     * @var string
+     */
     private $company;
 
+    /**
+     * Create a new employee instance
+     * @param  string $firstName
+     * @param  string $lastName
+     * @param  string $gender
+     * @param  string $job
+     * @param  string $company
+     * @return void
+     */
     public function __construct($firstName, $lastName, $gender = 'm',
         $job, $company)
     {
@@ -39,6 +87,10 @@ class Employee extends Person
         $this->company = $company;
     }
 
+    /**
+     * The (string) representation of this Person as "Work as [job] at
+     * [company]"
+     */
     public function __toString()
     {
         return 'Work as' . ' ' . $this->job . ' at ' . $this->company;
