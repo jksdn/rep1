8422012e54451d01d3c28b20156928addd92b169
Admin <admin@Admins-Mac-mini.local>
2019-11-25 02:20:03 +0200

    wip

--- Example.js
+++ Example.js
@@ -3,29 +3,29 @@ import { BrowserRouter, Route, useParams } from 'react-router-dom';
 import { render } from 'react-dom';
 
 export default function Example() {
-	return (
-		<div className="container">
-			<div className="row justifyContent-center">
-				<div className="col-md-8">
-					<button
-						className="c-button c-button--indigo c-button--medium"
-						type="button"
-					>
-						Button 1
-					</button>
+    return (
+        <div className="container">
+            <div className="row justifyContent-center">
+                <div className="col-md-8">
+                    <button
+                        className="c-button c-button--indigo c-button--medium"
+                        type="button"
+                    >
+                        Button 1
+                    </button>
 
-					<button
-						className="c-button c-button--purple c-button--medium"
-						type="button"
-					>
-						Button 2
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+                    <button
+                        className="c-button c-button--purple c-button--medium"
+                        type="button"
+                    >
+                        Button 2
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 if (document.getElementById('root')) {
-	render(<Example />, document.getElementById('root'));
+    render(<Example />, document.getElementById('root'));
 }
