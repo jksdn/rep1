e49b28d55ca9a427aa5e6c44784223a7a6448bb0
Admin <admin@Admins-Mac-mini.local>
2019-11-25 02:19:20 +0200

    wip

--- Example.js
+++ Example.js
@@ -7,13 +7,19 @@ export default function Example() {
 		<div className="container">
 			<div className="row justifyContent-center">
 				<div className="col-md-8">
-					<div className="c-card">
-						<h3>Example Component</h3>
-						<p>I'm an example component!</p>
+					<button
+						className="c-button c-button--indigo c-button--medium"
+						type="button"
+					>
+						Button 1
+					</button>
 
-						<button className="c-button c-button--indigo c-button--medium">Button 1</button>
-						<button className="c-button c-button--purple c-button--medium">Button 2</button>
-					</div>
+					<button
+						className="c-button c-button--purple c-button--medium"
+						type="button"
+					>
+						Button 2
+					</button>
 				</div>
 			</div>
 		</div>
