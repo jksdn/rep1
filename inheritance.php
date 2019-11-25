9a69d5bd67e3c2341f7ab3a90025bb13603026d2
Admin <admin@Admins-Mac-mini.local>
2019-11-25 03:00:46 +0200

    added inheritance.php

---
+++ inheritance.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Person.php
+ */
+class Person
+{
+    private $firstName;
+    private $lastName;
+    private $gender;
+
+    public function __construct($firstName, $lastName, $gender = 'm')
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->gender = $gender;
+    }
+
+    public function __toString()
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+}
+
+/**
+ * Employee.php
+ */
+class Employee extends Person
+{
+    private $jobTitle;
+    private $company;
+
+    public function __construct($firstName, $lastName, $gender = 'm',
+        $jobTitle, $company)
+    {
+        parent::__construct($firstName, $lastName, $gender);
+
+        $this->jobTitle = $jobTitle;
+        $this->company = $company;
+    }
+
+    public function __toString()
+    {
+        return 'Work as' . ' ' . $this->jobTitle . ' at ' . $this->company;
+    }
+}
+
+echo '<p>' . (new Employee('Mary', 'Doe', 'Frontend Developer', 'Google'))
+    . '</p>';
+
+echo '<p>' . (new Person('John', 'Doe')) . '</p>';
+echo '<p>' . (new Person('Jane', 'Doe')) . '</p>';
